99b1351f24b33950f1b3c3c7a20425960c21e745
Metre-Ray <dgary@tut.by>
2018-10-13 08:50:33 +0300

    remove comments

--- src/index.js
+++ src/index.js
@@ -54,23 +54,5 @@ class SmartCalculator {
 }
 
 
-/*resolve_chain2() {   // not used now
-  let values = [...this.values];
-  let methods = [...this.methods];
-  let value = this.values[0].toString();
-  for (let i = 0; i < this.methods.length; i++) { //pow
-    if (methods[i] === 'pow') {
-      if (values[2 * i + 1] === undefined) {
-        for (let j = 2 * i + 1; j < values.length; j++) {
-          //TODO find values !== undefined
-        }
-      }
-      value = values[2 * i] ** values[2 * i + 1];
-      methods.splice(i, 1);
-      //TODO values
-    }
-  }
-}*/
-
 
 module.exports = SmartCalculator;
